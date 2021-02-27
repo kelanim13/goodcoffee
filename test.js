f20ec4bf1aa94e514ba43dd0b6233de4c014e11c
kelanim13 <lkmuelle@gmail.com>
2021-02-27 15:33:22 -0600

    more changes to the coffee site!

---
+++ test.js
@@ -0,0 +1,8 @@
+let teachers = ['Assaf', 'Shane', 'Zack']
+for (let i = 0; i < teachers.length; i++) {
+    console.log(teachers[i]);
+}
+
+console.log('hello world');
+
+/* alert('this is a javascript alert') */
